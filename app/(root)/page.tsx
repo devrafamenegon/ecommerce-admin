8aaed29bb16c4451ecf1d24d332cd09ef2b15d88
Rafael Menegon <dev.rafaelmenegon@gmail.com>
2024-04-29 12:51:47 -0300

    feat: add user button from clerk into setup page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,9 @@
-export default function Home() {
+import { UserButton } from "@clerk/nextjs";
+
+export default function SetupPage() {
   return (
-    <p>Hello</p>
+    <div className="p-4">
+      <UserButton />
+    </div>
   );
 }
