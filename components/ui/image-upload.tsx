3ca95cbe20372814fb097f16bfb5c0fb05a20f33
Rafael Menegon <dev.rafaelmenegon@gmail.com>
2024-06-08 15:45:23 -0300

    feat: create image upload base ui component

---
+++ components/ui/image-upload.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+interface ImageUploadProps {
+  disabled?: boolean;
+  onChange: (value: string) => void;
+  onRemove: (value: string) => void;
+  value: string[]
+}
+
+const ImageUpload: React.FC<ImageUploadProps> = ({
+  disabled,
+  onChange,
+  onRemove,
+  value
+}) => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true)
+  }, []);
+
+  const onUpload = (result: any) => {
+    onChange(result.info.secure_url);
+  }
+
+  if (!isMounted) return null;
+
+  return (
+    <div></div>
+  )
+}
+
+export default ImageUpload;
