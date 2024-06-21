a031ba3c1f96f5f6bcdd716bde65bbaf574c735a
Rafael Menegon <dev.rafaelmenegon@gmail.com>
2024-06-21 00:51:53 -0300

    feat: extract search key to a parameter

--- components/ui/data-table.tsx
+++ components/ui/data-table.tsx
@@ -25,12 +25,14 @@ import { Input } from "@/components/ui/input"
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[]
-  data: TData[]
+  data: TData[],
+  searchKey: string
 }
 
 export function DataTable<TData, TValue>({
   columns,
   data,
+  searchKey
 }: DataTableProps<TData, TValue>) {
   const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>(
     []
@@ -52,10 +54,10 @@ export function DataTable<TData, TValue>({
     <div>
       <div className="flex items-center py-4">
         <Input
-          placeholder="Filter labels..."
-          value={(table.getColumn("label")?.getFilterValue() as string) ?? ""}
+          placeholder="Search..."
+          value={(table.getColumn(searchKey)?.getFilterValue() as string) ?? ""}
           onChange={(event) =>
-            table.getColumn("label")?.setFilterValue(event.target.value)
+            table.getColumn(searchKey)?.setFilterValue(event.target.value)
           }
           className="max-w-sm"
         />
