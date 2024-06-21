3ca238ad21996e1338aa308ea4bce23fe3782ea0
Rafael Menegon <dev.rafaelmenegon@gmail.com>
2024-06-21 00:03:06 -0300

    fix: change order of navbar

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -19,14 +19,14 @@ export function MainNav({
       active: pathname === `/${params.storeId}`
     },
     {
-      href: `/${params.storeId}/settings`,
-      label: 'Settings',
-      active: pathname === `/${params.storeId}/settings`
-    },
-    {
       href: `/${params.storeId}/billboards`,
       label: 'Billboards',
       active: pathname === `/${params.storeId}/billboards`
+    },
+    {
+      href: `/${params.storeId}/settings`,
+      label: 'Settings',
+      active: pathname === `/${params.storeId}/settings`
     }
   ];
   
