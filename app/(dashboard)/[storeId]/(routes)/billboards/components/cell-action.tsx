77fc16cf3dc006f051a9ccba38ea7553d7ae22b0
Rafael Menegon <dev.rafaelmenegon@gmail.com>
2024-06-21 01:11:11 -0300

    feat: create dropdown menu items

--- app/(dashboard)/[storeId]/(routes)/billboards/components/cell-action.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/cell-action.tsx
@@ -1,5 +1,10 @@
 "use client";
 
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { Copy, Edit, MoreHorizontal, Trash } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+
 import { BillboardColumn } from "./columns";
 
 interface CellActionProps {
@@ -10,6 +15,30 @@ export const CellAction: React.FC<CellActionProps> = ({
   data,
 }) => {
   return (
-    <div>Action</div>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="h-8 w-4 p-0">
+          <span className="sr-only">Open menu</span>
+          <MoreHorizontal className="h-4 w-4"/>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuLabel>
+          Actions
+        </DropdownMenuLabel>
+        <DropdownMenuItem>
+          <Copy className="mr-2 h-4 w-4" />
+          Copy Id
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <Edit className="mr-2 h-4 w-4" />
+          Update
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <Trash className="mr-2 h-4 w-4" />
+          Delete
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   )
 }
