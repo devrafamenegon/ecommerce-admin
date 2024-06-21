30cd752fb8bde1e514692c87145bb68e53b9c913
Rafael Menegon <dev.rafaelmenegon@gmail.com>
2024-06-21 00:41:19 -0300

    feat: add data table and pagination to it

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -6,7 +6,9 @@ import { useParams, useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
-import { BillboardColumn } from "./columns";
+import { DataTable } from "@/components/ui/data-table";
+
+import { BillboardColumn, columns } from "./columns";
 
 interface BillboardClientProps {
   data: BillboardColumn[]
@@ -31,6 +33,7 @@ export const BillboardClient: React.FC<BillboardClientProps> = ({
         </Button>
       </div>
       <Separator />
+      <DataTable columns={columns} data={data}/>
     </>
   )
 }
